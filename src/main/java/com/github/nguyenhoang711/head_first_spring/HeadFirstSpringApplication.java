25dd227fa66f3f8f79c65d9cc30620f6c61370fe
nguyenhoang711 <metmoilamroiday4@gmail.com>
2025-04-23 14:07:59 +0700

    initialize project

---
+++ src/main/java/com/github/nguyenhoang711/head_first_spring/HeadFirstSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.nguyenhoang711.head_first_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadFirstSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadFirstSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nguyenhoang711/head_first_spring/HeadFirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nguyenhoang711.head_first_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadFirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
